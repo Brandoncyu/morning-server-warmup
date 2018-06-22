363879c509658dd42cba6535d994a6d5f9baa4ab
Brandon <brandoncyu@gmail.com>
2018-06-22 10:21:42 -0700

    fixed stuff

--- app.js
+++ app.js
@@ -9,17 +9,33 @@ app.use(morgan('dev'))
 app.use(bodyParser.json())
 
 app.get('/ping', (req, res, next) =>{
-  res.send('PONG!')
-})
-
-app.use((err, req, res, next) =>{
-  const status = err.status || 500
-  res.status(status).json({error: 'Internal Server Error'})
+  res.status(200).send({message: 'PONG!'})
 })
 
 app.use((req,res,next) =>{
-  res.status(404).json({message: 'Not Found'})
+  const status = 404
+  const message = `Could not ${req.method} ${req.url}`
+
+  next({ status, message })
 })
+
+app.use((err, req, res, next) =>{
+  console.error(err)
+  const errorMessage = {}
+
+  errorMessage.status = err.status || 500
+  errorMessage.message = err.message || 'Something went wrong'
+
+  if(process.env.NODE_ENV !== 'production'){
+    errorMessage.stack = err.stack
+  }
+
+  res.status(errorMessage.status).send(errorMessage)
+
+  // const status = err.status || 500
+  // res.status(status).json({error: 'Something went wrong'})
+})
+
 
 const listener = () => console.log(`Listening on port ${port}`)
 app.listen(port, listener)
